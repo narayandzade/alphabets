3c35b12e799c35f84f5feac97631774fbf61d84e
Narayan Zade <narayan.d.zade@gmail.com>
2026-06-17 16:01:03 +0530

    first push

--- index.php
+++ index.php
@@ -7,27 +7,22 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
 <div class="app">
-
   <header class="app-header">
     <div class="header-title">Alphabet Handwriting</div>
     <div class="header-sub">Click any character to animate stroke order</div>
   </header>
-
   <div class="board-wrapper">
     <div class="board">
       <div class="line line-red  line-top"></div>
-      <div class="line line-gray line-cap"></div>
-      <div class="line line-gray line-base"></div>
+      <div class="line line-blue line-cap"></div>
+      <div class="line line-blue line-base"></div>
       <div class="line line-red  line-desc"></div>
       <svg id="canvas" viewBox="0 0 900 300" xmlns="http://www.w3.org/2000/svg"></svg>
     </div>
     <div class="board-label" id="board-label">Click a character to begin</div>
   </div>
-
   <div class="panels-row">
-
     <div class="char-panel">
       <div class="panel-header">
         <span class="panel-label">A – Z</span>
@@ -35,7 +30,6 @@
       </div>
       <div class="char-grid" id="grid-upper"></div>
     </div>
-
     <div class="char-panel">
       <div class="panel-header">
         <span class="panel-label">a – z</span>
@@ -43,7 +37,6 @@
       </div>
       <div class="char-grid" id="grid-lower"></div>
     </div>
-
     <div class="char-panel num-panel">
       <div class="panel-header">
         <span class="panel-label">0 – 100</span>
@@ -51,11 +44,8 @@
       </div>
       <div class="char-grid num-grid" id="grid-nums"></div>
     </div>
-
   </div>
-
 </div>
-
 <script src="script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 const canvas = document.getElementById('canvas');
 const label  = document.getElementById('board-label');
-
-const ns = 'http://www.w3.org/2000/svg';
+const ns     = 'http://www.w3.org/2000/svg';
 
 const VW   = 900;
-const TOP  = 30;
-const XHT  = 80;
-const BASE = 200;
-const DESC = 250;
-const CX   = 400;
+const TOP  = 40;
+const CAP  = 100;
+const BASE = 220;
+const DESC = 280;
+const CX   = 450;
 
 const MID_U = Math.round((TOP  + BASE) / 2);
-const MID_L = Math.round((XHT  + BASE) / 2);
-
-function el(tag, attrs) {
-  const e = document.createElementNS(ns, tag);
-  for (const k in attrs) e.setAttribute(k, attrs[k]);
-  return e;
-}
+const MID_L = Math.round((CAP  + BASE) / 2);
 
 function smooth(pts) {
   if (pts.length < 2) return `M${pts[0][0]},${pts[0][1]}`;
@@ -35,247 +28,266 @@ function smooth(pts) {
 const STROKES = {
   A: {
     upper: [
-      [[CX, TOP],        [CX-65, BASE]],
-      [[CX, TOP],        [CX+65, BASE]],
-      [[CX-38, MID_U+10],[CX+38, MID_U+10]]
+      [[CX, TOP], [CX-70, BASE]],
+      [[CX, TOP], [CX+70, BASE]],
+      [[CX-40, MID_U+10], [CX+40, MID_U+10]]
     ],
     lower: [
-      [[CX+42, XHT+5], [CX+42, XHT], [CX+24, XHT-5], [CX, XHT], [CX-18, XHT+20], [CX-18, XHT+60], [CX, BASE], [CX+24, BASE-4], [CX+42, MID_L+25]],
-      [[CX+42, MID_L+25], [CX+42, BASE]]
+      [[CX+10, CAP+5], [CX-15, CAP+5], [CX-38, CAP+30], [CX-38, MID_L+20], [CX-15, BASE], [CX+10, BASE], [CX+32, MID_L+20], [CX+32, CAP+5]],
+      [[CX+32, CAP+5], [CX+32, BASE]]
     ]
   },
   B: {
     upper: [
-      [[CX-45, TOP],  [CX-45, BASE]],
-      [[CX-45, TOP],  [CX+20, TOP],   [CX+50, TOP+25],  [CX+50, TOP+55],  [CX+20, MID_U],  [CX-45, MID_U]],
-      [[CX-45, MID_U],[CX+25, MID_U], [CX+55, MID_U+28],[CX+55, MID_U+58],[CX+25, BASE],   [CX-45, BASE]]
+      [[CX-50, TOP], [CX-50, BASE]],
+      [[CX-50, TOP], [CX+20, TOP], [CX+55, TOP+28], [CX+55, MID_U-5], [CX+20, MID_U], [CX-50, MID_U]],
+      [[CX-50, MID_U], [CX+25, MID_U], [CX+58, MID_U+28], [CX+58, BASE-22], [CX+25, BASE], [CX-50, BASE]]
     ],
     lower: [
-      [[CX-22, TOP],  [CX-22, BASE]],
-      [[CX-22, MID_L],[CX+5,  XHT],   [CX+30, XHT],     [CX+48, MID_L+5], [CX+48, MID_L+38],[CX+30, BASE],[CX+5, BASE],[CX-22, MID_L+42]]
+      [[CX-25, TOP], [CX-25, BASE]],
+      [[CX-25, MID_L-5], [CX+2, CAP+2], [CX+28, CAP+2], [CX+48, CAP+22], [CX+48, MID_L+18], [CX+28, BASE], [CX+2, BASE], [CX-25, MID_L+20]]
     ]
   },
   C: {
     upper: [
-      [[CX+55, TOP+22],[CX+30, TOP],   [CX-5,  TOP],     [CX-44, TOP+34],  [CX-56, MID_U],  [CX-44, MID_U+58],[CX-5,BASE],[CX+30,BASE],[CX+55,BASE-22]]
+      [[CX+60, TOP+25], [CX+32, TOP], [CX-2, TOP], [CX-44, TOP+36], [CX-58, MID_U], [CX-44, BASE-36], [CX-2, BASE], [CX+32, BASE], [CX+60, BASE-25]]
     ],
     lower: [
-      [[CX+48, XHT+8],[CX+26, XHT],   [CX,    XHT],     [CX-22, XHT+20],  [CX-32, MID_L+10],[CX-22, BASE-5],[CX,BASE],[CX+26,BASE],[CX+48,BASE-12]]
+      [[CX+46, CAP+14], [CX+24, CAP+2], [CX, CAP+2], [CX-22, CAP+22], [CX-32, MID_L+10], [CX-22, BASE-8], [CX, BASE], [CX+24, BASE], [CX+46, BASE-14]]
     ]
   },
   D: {
     upper: [
-      [[CX-45, TOP],  [CX-45, BASE]],
-      [[CX-45, TOP],  [CX+15, TOP],   [CX+58, TOP+40],  [CX+58, MID_U+20],[CX+15, BASE],   [CX-45, BASE]]
+      [[CX-50, TOP], [CX-50, BASE]],
+      [[CX-50, TOP], [CX+18, TOP], [CX+62, TOP+45], [CX+62, BASE-45], [CX+18, BASE], [CX-50, BASE]]
     ],
     lower: [
-      [[CX+42, TOP],  [CX+42, BASE]],
-      [[CX+42, MID_L+2],[CX+18,XHT],  [CX-8,  XHT],     [CX-30, XHT+20],  [CX-30, MID_L+35],[CX-8,BASE],[CX+18,BASE],[CX+42,MID_L+42]]
+      [[CX+38, TOP], [CX+38, BASE]],
+      [[CX+38, MID_L], [CX+15, CAP+2], [CX-8, CAP+2], [CX-32, CAP+22], [CX-32, MID_L+35], [CX-8, BASE], [CX+15, BASE], [CX+38, MID_L+30]]
     ]
   },
   E: {
     upper: [
-      [[CX+50, TOP],  [CX-45, TOP],   [CX-45, BASE],    [CX+50, BASE]],
-      [[CX-45, MID_U],[CX+32, MID_U]]
+      [[CX+55, TOP], [CX-50, TOP], [CX-50, BASE], [CX+55, BASE]],
+      [[CX-50, MID_U], [CX+36, MID_U]]
     ],
     lower: [
-      [[CX-28, MID_L+22],[CX+36,MID_L+22],[CX+44,MID_L+5],[CX+30,XHT],[CX+4,XHT],[CX-20,XHT+20],[CX-28,MID_L+22],[CX-20,BASE-5],[CX+4,BASE],[CX+30,BASE],[CX+44,BASE-12]]
+      [[CX-30, MID_L+5], [CX+36, MID_L+5]],
+      [[CX+36, MID_L+5], [CX+36, CAP+10], [CX+10, CAP+2], [CX-14, CAP+10], [CX-30, CAP+30], [CX-30, MID_L+30], [CX-14, BASE-8], [CX+10, BASE], [CX+36, BASE-14]]
     ]
   },
   F: {
     upper: [
-      [[CX+50, TOP],  [CX-45, TOP],   [CX-45, BASE]],
-      [[CX-45, MID_U],[CX+32, MID_U]]
+      [[CX+55, TOP], [CX-50, TOP], [CX-50, BASE]],
+      [[CX-50, MID_U], [CX+36, MID_U]]
     ],
     lower: [
-      [[CX+30, XHT+10],[CX+14,XHT],   [CX-4,  XHT+12],  [CX-4,  DESC]],
-      [[CX-24, MID_L], [CX+20, MID_L]]
+      [[CX+30, CAP+10], [CX+14, CAP+2], [CX-2, CAP+14], [CX-2, BASE+20]],
+      [[CX-22, MID_L], [CX+22, MID_L]]
     ]
   },
   G: {
     upper: [
-      [[CX+55, TOP+22],[CX+30, TOP],   [CX-5,  TOP],     [CX-44, TOP+34],  [CX-56, MID_U],  [CX-44,MID_U+58],[CX-5,BASE],[CX+30,BASE],[CX+55,BASE-22],[CX+55,MID_U+10],[CX+12,MID_U+10]]
+      [[CX+60, TOP+25], [CX+32, TOP], [CX-2, TOP], [CX-44, TOP+36], [CX-58, MID_U], [CX-44, BASE-36], [CX-2, BASE], [CX+32, BASE], [CX+60, BASE-25], [CX+60, MID_U+5], [CX+14, MID_U+5]]
     ],
     lower: [
-      [[CX+48, XHT+8],[CX+26, XHT],   [CX,    XHT],     [CX-22, XHT+20],  [CX-32, MID_L+10],[CX-22,BASE-5],[CX,BASE],[CX+26,BASE],[CX+48,BASE-12],[CX+48,DESC-10],[CX+26,DESC],[CX,DESC-5]]
+      [[CX+46, CAP+14], [CX+24, CAP+2], [CX, CAP+2], [CX-22, CAP+22], [CX-32, MID_L+10], [CX-22, BASE-8], [CX, BASE], [CX+24, BASE], [CX+46, BASE-12], [CX+46, DESC-10], [CX+24, DESC+5], [CX, DESC]]
     ]
   },
   H: {
     upper: [
-      [[CX-45, TOP],  [CX-45, BASE]],
-      [[CX+45, TOP],  [CX+45, BASE]],
-      [[CX-45, MID_U],[CX+45, MID_U]]
+      [[CX-50, TOP], [CX-50, BASE]],
+      [[CX+50, TOP], [CX+50, BASE]],
+      [[CX-50, MID_U], [CX+50, MID_U]]
     ],
     lower: [
-      [[CX-22, TOP],  [CX-22, BASE]],
-      [[CX-22, MID_L+5],[CX+4,XHT],   [CX+30, XHT],     [CX+46, MID_L+8], [CX+46, BASE]]
+      [[CX-25, TOP], [CX-25, BASE]],
+      [[CX-25, MID_L+5], [CX+2, CAP+2], [CX+30, CAP+2], [CX+48, CAP+20], [CX+48, BASE]]
     ]
   },
   I: {
     upper: [
-      [[CX-35, TOP],  [CX+35, TOP]],
-      [[CX, TOP],     [CX, BASE]],
-      [[CX-35, BASE], [CX+35, BASE]]
+      [[CX-36, TOP], [CX+36, TOP]],
+      [[CX, TOP], [CX, BASE]],
+      [[CX-36, BASE], [CX+36, BASE]]
     ],
     lower: [
-      [[CX, XHT+2],   [CX, XHT+4]],
-      [[CX, XHT+14],  [CX, BASE]]
+      [[CX, CAP+2], [CX, CAP+6]],
+      [[CX, CAP+16], [CX, BASE]]
     ]
   },
   J: {
     upper: [
-      [[CX-15, TOP],  [CX+38, TOP]],
-      [[CX+12, TOP],  [CX+12, MID_U+52],[CX-5,BASE],    [CX-30, BASE-5],  [CX-42, MID_U+40]]
+      [[CX-18, TOP], [CX+40, TOP]],
+      [[CX+14, TOP], [CX+14, BASE-25], [CX+2, BASE], [CX-24, BASE-5], [CX-40, BASE-28]]
     ],
     lower: [
-      [[CX+10, XHT+2],[CX+10, XHT+4]],
-      [[CX+10, XHT+14],[CX+10,MID_L+50],[CX-5,DESC],    [CX-22, DESC-8],  [CX-30, DESC-22]]
+      [[CX+12, CAP+2], [CX+12, CAP+6]],
+      [[CX+12, CAP+16], [CX+12, DESC-10], [CX-2, DESC+5], [CX-20, DESC], [CX-28, DESC-18]]
     ]
   },
   K: {
     upper: [
-      [[CX-45, TOP],  [CX-45, BASE]],
-      [[CX+50, TOP],  [CX-45, MID_U]],
-      [[CX-45, MID_U],[CX+50, BASE]]
+      [[CX-50, TOP], [CX-50, BASE]],
+      [[CX+55, TOP], [CX-50, MID_U]],
+      [[CX-50, MID_U], [CX+55, BASE]]
     ],
     lower: [
-      [[CX-22, TOP],  [CX-22, BASE]],
-      [[CX+40, XHT],  [CX-22, MID_L+20]],
-      [[CX-22, MID_L+20],[CX+40,BASE]]
+      [[CX-25, TOP], [CX-25, BASE]],
+      [[CX+42, CAP+2], [CX-25, MID_L+18]],
+      [[CX-25, MID_L+18], [CX+42, BASE]]
     ]
   },
   L: {
     upper: [
-      [[CX-35, TOP],  [CX-35, BASE],   [CX+45, BASE]]
+      [[CX-38, TOP], [CX-38, BASE], [CX+48, BASE]]
     ],
     lower: [
-      [[CX-12, TOP],  [CX-12, BASE],   [CX+32, BASE]]
+      [[CX-14, TOP], [CX-14, BASE], [CX+34, BASE]]
     ]
   },
   M: {
     upper: [
-      [[CX-60, BASE], [CX-60, TOP],    [CX, MID_U+18],   [CX+60, TOP],     [CX+60, BASE]]
+      [[CX-65, BASE], [CX-65, TOP], [CX, MID_U+20], [CX+65, TOP], [CX+65, BASE]]
     ],
     lower: [
-      [[CX-30, BASE], [CX-30, XHT]],
-      [[CX-30, XHT],  [CX-10, XHT],    [CX+4,  MID_L+5], [CX+4,  BASE]],
-      [[CX+4,  XHT],  [CX+24, XHT],    [CX+40, MID_L+5], [CX+40, BASE]]
+      [[CX-32, BASE], [CX-32, CAP+2]],
+      [[CX-32, CAP+2], [CX-12, CAP+2], [CX+4, MID_L+8], [CX+4, BASE]],
+      [[CX+4, CAP+2], [CX+24, CAP+2], [CX+42, MID_L+8], [CX+42, BASE]]
     ]
   },
   N: {
     upper: [
-      [[CX-50, BASE], [CX-50, TOP],    [CX+50, BASE],    [CX+50, TOP]]
+      [[CX-55, BASE], [CX-55, TOP], [CX+55, BASE], [CX+55, TOP]]
     ],
     lower: [
-      [[CX-24, BASE], [CX-24, XHT]],
-      [[CX-24, XHT],  [CX+4,  XHT],    [CX+40, MID_L+18],[CX+40, XHT]],
-      [[CX+40, XHT],  [CX+40, BASE]]
+      [[CX-26, BASE], [CX-26, CAP+2]],
+      [[CX-26, CAP+2], [CX+5, CAP+2], [CX+42, MID_L+20], [CX+42, CAP+2]],
+      [[CX+42, CAP+2], [CX+42, BASE]]
     ]
   },
   O: {
     upper: [
-      [[CX, TOP],[CX+50,TOP+28],[CX+62,MID_U],[CX+50,MID_U+58],[CX,BASE],[CX-50,MID_U+58],[CX-62,MID_U],[CX-50,TOP+28],[CX,TOP]]
+      [[CX, TOP], [CX+55, TOP+30], [CX+68, MID_U], [CX+55, BASE-30], [CX, BASE], [CX-55, BASE-30], [CX-68, MID_U], [CX-55, TOP+30], [CX, TOP]]
     ],
     lower: [
-      [[CX,XHT],[CX+36,XHT],[CX+48,MID_L+18],[CX+48,MID_L+38],[CX+36,BASE],[CX,BASE],[CX-36,BASE],[CX-48,MID_L+38],[CX-48,MID_L+18],[CX-36,XHT],[CX,XHT]]
+      [[CX, CAP+2], [CX+38, CAP+10], [CX+50, MID_L+15], [CX+50, MID_L+35], [CX+38, BASE], [CX, BASE], [CX-38, BASE], [CX-50, MID_L+35], [CX-50, MID_L+15], [CX-38, CAP+10], [CX, CAP+2]]
     ]
   },
   P: {
     upper: [
-      [[CX-45, TOP],  [CX-45, BASE]],
-      [[CX-45, TOP],  [CX+18, TOP],    [CX+50, TOP+26],  [CX+50, MID_U-18],[CX+18, MID_U],  [CX-45, MID_U]]
+      [[CX-50, TOP], [CX-50, BASE]],
+      [[CX-50, TOP], [CX+20, TOP], [CX+55, TOP+28], [CX+55, MID_U-5], [CX+20, MID_U], [CX-50, MID_U]]
     ],
     lower: [
-      [[CX-22, XHT],  [CX-22, DESC]],
-      [[CX-22, XHT],  [CX+8,  XHT],    [CX+42, XHT+18],  [CX+42, MID_L+35],[CX+8,  BASE],   [CX-22, BASE]]
+      [[CX-25, CAP+2], [CX-25, DESC+5]],
+      [[CX-25, CAP+2], [CX+8, CAP+2], [CX+44, CAP+22], [CX+44, MID_L+32], [CX+8, BASE], [CX-25, BASE]]
     ]
   },
   Q: {
     upper: [
-      [[CX,TOP],[CX+50,TOP+28],[CX+62,MID_U],[CX+50,MID_U+58],[CX,BASE],[CX-50,MID_U+58],[CX-62,MID_U],[CX-50,TOP+28],[CX,TOP]],
-      [[CX+24, MID_U+45],[CX+60, BASE+22]]
+      [[CX, TOP], [CX+55, TOP+30], [CX+68, MID_U], [CX+55, BASE-30], [CX, BASE], [CX-55, BASE-30], [CX-68, MID_U], [CX-55, TOP+30], [CX, TOP]],
+      [[CX+26, MID_U+48], [CX+64, BASE+25]]
     ],
     lower: [
-      [[CX,XHT],[CX+36,XHT],[CX+48,MID_L+18],[CX+48,MID_L+38],[CX+36,BASE],[CX,BASE],[CX-36,BASE],[CX-48,MID_L+38],[CX-48,MID_L+18],[CX-36,XHT],[CX,XHT]],
-      [[CX+24, MID_L+42],[CX+52, DESC]]
+      [[CX, CAP+2], [CX+38, CAP+10], [CX+50, MID_L+15], [CX+50, MID_L+35], [CX+38, BASE], [CX, BASE], [CX-38, BASE], [CX-50, MID_L+35], [CX-50, MID_L+15], [CX-38, CAP+10], [CX, CAP+2]],
+      [[CX+26, MID_L+42], [CX+54, DESC+5]]
     ]
   },
   R: {
     upper: [
-      [[CX-45, TOP],  [CX-45, BASE]],
-      [[CX-45, TOP],  [CX+18, TOP],    [CX+50, TOP+26],  [CX+50, MID_U-18],[CX+18, MID_U],  [CX-45, MID_U]],
-      [[CX+8,  MID_U],[CX+55, BASE]]
+      [[CX-50, TOP], [CX-50, BASE]],
+      [[CX-50, TOP], [CX+20, TOP], [CX+55, TOP+28], [CX+55, MID_U-5], [CX+20, MID_U], [CX-50, MID_U]],
+      [[CX+10, MID_U], [CX+58, BASE]]
     ],
     lower: [
-      [[CX-24, XHT],  [CX-24, BASE]],
-      [[CX-24, MID_L+8],[CX,XHT],      [CX+22, XHT],     [CX+36, MID_L+5], [CX+30, MID_L+22],[CX-24,MID_L+25]]
+      [[CX-26, CAP+2], [CX-26, BASE]],
+      [[CX-26, CAP+2], [CX, CAP+2], [CX+24, CAP+10], [CX+24, MID_L+5], [CX-26, MID_L+10]]
     ]
   },
   S: {
     upper: [
-      [[CX+50,TOP+22],[CX+24,TOP],[CX-8,TOP],[CX-40,TOP+26],[CX-40,MID_U-15],[CX,MID_U],[CX+40,MID_U+15],[CX+40,BASE-26],[CX+8,BASE],[CX-24,BASE],[CX-50,BASE-22]]
+      [[CX+55, TOP+25], [CX+26, TOP], [CX-6, TOP], [CX-42, TOP+28], [CX-42, MID_U-15], [CX, MID_U], [CX+42, MID_U+15], [CX+42, BASE-28], [CX+6, BASE], [CX-26, BASE], [CX-55, BASE-25]]
     ],
     lower: [
-      [[CX+38,XHT+8],[CX+20,XHT],[CX-2,XHT],[CX-22,XHT+18],[CX-22,MID_L+5],[CX,MID_L+18],[CX+22,MID_L+32],[CX+22,BASE-8],[CX+2,BASE],[CX-20,BASE],[CX-38,BASE-10]]
+      [[CX+38, CAP+12], [CX+18, CAP+2], [CX-4, CAP+2], [CX-24, CAP+20], [CX-24, MID_L+5], [CX, MID_L+18], [CX+24, MID_L+32], [CX+24, BASE-10], [CX+4, BASE], [CX-18, BASE], [CX-38, BASE-14]]
     ]
   },
   T: {
     upper: [
-      [[CX-55, TOP],  [CX+55, TOP]],
-      [[CX, TOP],     [CX, BASE]]
+      [[CX-60, TOP], [CX+60, TOP]],
+      [[CX, TOP], [CX, BASE]]
     ],
     lower: [
-      [[CX-10, XHT+8],[CX-10, BASE+2], [CX+4,  BASE+8]],
-      [[CX-30, MID_L],[CX+20, MID_L]]
+      [[CX-12, CAP+10], [CX-12, BASE+5], [CX+5, BASE+10]],
+      [[CX-32, MID_L], [CX+22, MID_L]]
     ]
   },
   U: {
     upper: [
-      [[CX-50,TOP],[CX-50,MID_U+52],[CX-28,BASE],[CX,BASE+5],[CX+28,BASE],[CX+50,MID_U+52],[CX+50,TOP]]
+      [[CX-55, TOP], [CX-55, BASE-30], [CX-30, BASE], [CX, BASE+5], [CX+30, BASE], [CX+55, BASE-30], [CX+55, TOP]]
     ],
     lower: [
-      [[CX-24,XHT],[CX-24,MID_L+45],[CX-8,BASE],[CX+14,BASE],[CX+40,MID_L+38],[CX+40,XHT]]
+      [[CX-26, CAP+2], [CX-26, MID_L+45], [CX-8, BASE], [CX+16, BASE], [CX+42, MID_L+38], [CX+42, CAP+2]]
     ]
   },
   V: {
-    upper: [[[CX-55, TOP], [CX, BASE], [CX+55, TOP]]],
-    lower:  [[[CX-32, XHT],[CX, BASE], [CX+32, XHT]]]
+    upper: [
+      [[CX-58, TOP], [CX, BASE], [CX+58, TOP]]
+    ],
+    lower: [
+      [[CX-34, CAP+2], [CX, BASE], [CX+34, CAP+2]]
+    ]
   },
   W: {
-    upper: [[[CX-60,TOP],[CX-30,BASE],[CX,MID_U+28],[CX+30,BASE],[CX+60,TOP]]],
-    lower:  [[[CX-40,XHT],[CX-20,BASE],[CX,MID_L+28],[CX+20,BASE],[CX+40,XHT]]]
+    upper: [
+      [[CX-65, TOP], [CX-32, BASE], [CX, MID_U+28], [CX+32, BASE], [CX+65, TOP]]
+    ],
+    lower: [
+      [[CX-42, CAP+2], [CX-22, BASE], [CX, MID_L+28], [CX+22, BASE], [CX+42, CAP+2]]
+    ]
   },
   X: {
-    upper: [[[CX-50,TOP],[CX+50,BASE]],[[CX+50,TOP],[CX-50,BASE]]],
-    lower:  [[[CX-30,XHT],[CX+30,BASE]],[[CX+30,XHT],[CX-30,BASE]]]
+    upper: [
+      [[CX-55, TOP], [CX+55, BASE]],
+      [[CX+55, TOP], [CX-55, BASE]]
+    ],
+    lower: [
+      [[CX-32, CAP+2], [CX+32, BASE]],
+      [[CX+32, CAP+2], [CX-32, BASE]]
+    ]
   },
   Y: {
     upper: [
-      [[CX-50, TOP],  [CX, MID_U]],
-      [[CX+50, TOP],  [CX, MID_U], [CX, BASE]]
+      [[CX-55, TOP], [CX, MID_U]],
+      [[CX+55, TOP], [CX, MID_U], [CX, BASE]]
     ],
     lower: [
-      [[CX-30, XHT],  [CX, MID_L+22]],
-      [[CX+30, XHT],  [CX, MID_L+22],[CX, DESC]]
+      [[CX-32, CAP+2], [CX, MID_L+22]],
+      [[CX+32, CAP+2], [CX, MID_L+22], [CX, DESC+5]]
     ]
   },
   Z: {
-    upper: [[[CX-50,TOP],[CX+50,TOP],[CX-50,BASE],[CX+50,BASE]]],
-    lower:  [[[CX-30,XHT],[CX+30,XHT],[CX-30,BASE],[CX+30,BASE]]]
+    upper: [
+      [[CX-55, TOP], [CX+55, TOP], [CX-55, BASE], [CX+55, BASE]]
+    ],
+    lower: [
+      [[CX-32, CAP+2], [CX+32, CAP+2], [CX-32, BASE], [CX+32, BASE]]
+    ]
   }
 };
 
 const DIGIT_STROKES = {
-  '0': [[[CX,TOP],[CX+52,TOP+28],[CX+62,MID_U],[CX+52,MID_U+58],[CX,BASE],[CX-52,MID_U+58],[CX-62,MID_U],[CX-52,TOP+28],[CX,TOP]]],
-  '1': [[[CX-15,TOP+25],[CX+10,TOP],[CX+10,BASE]],[[CX-20,BASE],[CX+35,BASE]]],
-  '2': [[[CX-40,TOP+25],[CX-20,TOP],[CX+20,TOP],[CX+44,TOP+25],[CX+44,MID_U-10],[CX-40,BASE],[CX+46,BASE]]],
-  '3': [[[CX-40,TOP+15],[CX,TOP],[CX+38,TOP+20],[CX+38,MID_U-10],[CX,MID_U]],[[CX,MID_U],[CX+42,MID_U+15],[CX+42,BASE-15],[CX,BASE],[CX-40,BASE-15]]],
-  '4': [[[CX+28,BASE],[CX+28,TOP],[CX-40,MID_U+15],[CX+46,MID_U+15]]],
-  '5': [[[CX+38,TOP],[CX-36,TOP],[CX-40,MID_U-5],[CX+10,MID_U-5],[CX+42,MID_U+20],[CX+42,BASE-20],[CX+10,BASE],[CX-36,BASE-15]]],
-  '6': [[[CX+38,TOP+18],[CX+15,TOP],[CX-15,TOP],[CX-44,TOP+30],[CX-50,MID_U],[CX-44,MID_U+55],[CX-10,BASE],[CX+20,BASE],[CX+46,MID_U+55],[CX+46,MID_U+18],[CX+20,MID_U-5],[CX-10,MID_U-5],[CX-44,MID_U+18]]],
-  '7': [[[CX-42,TOP],[CX+46,TOP],[CX-10,BASE]]],
-  '8': [[[CX,MID_U],[CX+40,TOP+40],[CX+40,TOP+20],[CX+22,TOP],[CX,TOP],[CX-22,TOP],[CX-40,TOP+20],[CX-40,TOP+40],[CX,MID_U],[CX+44,MID_U+30],[CX+44,BASE-20],[CX+22,BASE],[CX,BASE],[CX-22,BASE],[CX-44,BASE-20],[CX-44,MID_U+30],[CX,MID_U]]],
-  '9': [[[CX-46,BASE-18],[CX-22,BASE],[CX+10,BASE],[CX+44,BASE-30],[CX+50,MID_U],[CX+44,TOP+38],[CX+10,TOP+5],[CX-22,TOP+5],[CX-44,TOP+38],[CX-44,MID_U+18],[CX-22,MID_U+5],[CX+10,MID_U+5],[CX+44,MID_U+18]]]
+  '0': [[[CX,TOP],[CX+55,TOP+30],[CX+65,MID_U],[CX+55,BASE-30],[CX,BASE],[CX-55,BASE-30],[CX-65,MID_U],[CX-55,TOP+30],[CX,TOP]]],
+  '1': [[[CX-18,TOP+28],[CX+10,TOP],[CX+10,BASE]],[[CX-22,BASE],[CX+38,BASE]]],
+  '2': [[[CX-42,TOP+28],[CX-22,TOP],[CX+22,TOP],[CX+46,TOP+28],[CX+46,MID_U-8],[CX-42,BASE],[CX+48,BASE]]],
+  '3': [[[CX-42,TOP+18],[CX,TOP],[CX+40,TOP+22],[CX+40,MID_U-10],[CX,MID_U]],[[CX,MID_U],[CX+44,MID_U+18],[CX+44,BASE-18],[CX,BASE],[CX-42,BASE-18]]],
+  '4': [[[CX+30,BASE],[CX+30,TOP],[CX-42,MID_U+18],[CX+48,MID_U+18]]],
+  '5': [[[CX+40,TOP],[CX-38,TOP],[CX-42,MID_U-5],[CX+12,MID_U-5],[CX+44,MID_U+22],[CX+44,BASE-22],[CX+12,BASE],[CX-38,BASE-18]]],
+  '6': [[[CX+40,TOP+20],[CX+16,TOP],[CX-16,TOP],[CX-46,TOP+32],[CX-52,MID_U],[CX-46,BASE-28],[CX-10,BASE],[CX+22,BASE],[CX+48,BASE-28],[CX+48,MID_U+20],[CX+22,MID_U-5],[CX-10,MID_U-5],[CX-46,MID_U+20]]],
+  '7': [[[CX-44,TOP],[CX+48,TOP],[CX-8,BASE]]],
+  '8': [[[CX,MID_U],[CX+42,TOP+42],[CX+42,TOP+22],[CX+24,TOP],[CX,TOP],[CX-24,TOP],[CX-42,TOP+22],[CX-42,TOP+42],[CX,MID_U],[CX+46,MID_U+32],[CX+46,BASE-22],[CX+24,BASE],[CX,BASE],[CX-24,BASE],[CX-46,BASE-22],[CX-46,MID_U+32],[CX,MID_U]]],
+  '9': [[[CX-48,BASE-20],[CX-24,BASE],[CX+10,BASE],[CX+46,BASE-32],[CX+52,MID_U],[CX+46,TOP+40],[CX+10,TOP+5],[CX-24,TOP+5],[CX-46,TOP+40],[CX-46,MID_U+20],[CX-24,MID_U+5],[CX+10,MID_U+5],[CX+46,MID_U+20]]]]
 };
 
 function makeMultiDigitStrokes(numStr) {
@@ -339,11 +351,11 @@ function startStrokes(strokes, displayChar) {
   if (!strokes || strokes.length === 0) return;
 
   const bgEl = svgEl('text', {
-    x: CX, y: BASE + 15,
-    'font-size': 210,
-    'font-family': 'Georgia, serif',
+    x: CX, y: BASE + 18,
+    'font-size': 230,
+    'font-family': 'Arial, sans-serif',
     fill: '#e8eaf6',
-    opacity: 0.07,
+    opacity: 0.06,
     'text-anchor': 'middle'
   });
   bgEl.textContent = displayChar;
@@ -376,14 +388,6 @@ function startStrokes(strokes, displayChar) {
   const cursor = svgEl('circle', { cx: -30, cy: -30, r: 9, fill: '#e53935' });
   canvas.appendChild(cursor);
   canvas._cursor = cursor;
-
-  const nameEl = svgEl('text', {
-    x: VW - 55, y: BASE + 10,
-    'font-size': 26, 'font-family': 'Georgia, serif',
-    fill: '#3344aa', opacity: 0.45, 'text-anchor': 'middle'
-  });
-  nameEl.textContent = displayChar;
-  canvas.appendChild(nameEl);
 
   label.textContent = `Writing "${displayChar}" — ${strokes.length} stroke${strokes.length !== 1 ? 's' : ''}`;
 
